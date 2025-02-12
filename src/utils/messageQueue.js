e0a8a7d386b37d37367ca8405b7ba849c7e73478
RachitShahrawat <rachitshahrawat@gmail.com>
2025-02-12 23:33:16 +0530

    fixed some bug

--- src/utils/messageQueue.js
+++ src/utils/messageQueue.js
@@ -6,6 +6,7 @@ const createChannel=async()=>{
         const connection=await amqplib.connect(MESSAGE_BROKER_URL);
         const channel=await connection.createChannel();
         await channel.assertExchange(EXCHANGE_NAME,'direct',false);
+        return channel;
     }
     catch(error){
         throw error;
